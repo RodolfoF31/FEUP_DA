a0cb2d4913428e39f687b72497ee78d8a60d5de8
joao <up202106996@up.pt>
2023-04-01 20:07:03 +0100

    T2.1 working

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include "Utils/MenuMan.h"
 #include "Utils/FileMan.h"
 #include "Models/Graph.h"
@@ -6,17 +7,27 @@ Graph graph;
 
 void displayMainMenu(){
     while (true){
-        int choice = MenuMan::createMenu("\nSelect what you pretend to do: ", {"Find shortest path","Exit"});
+        int choice = MenuMan::createMenu("\nSelect what you pretend to do: ", {"Find shortest path","Find maximum number of trains","Exit"});
         string source, destination;
+        int max_trains;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
         switch (choice) {
             case 1:
                 cout << "\nEnter the source station name: ";
-                cin >> source;
+                getline(cin, source);
                 cout << "\nEnter the destination station name: ";
-                cin >> destination;
+                getline(cin, destination);
                 graph.dijkstra(source, destination);
                 break;
             case 2:
+                cout << "\nEnter the source station name: ";
+                getline(cin, source);
+                cout << "\nEnter the destination station name: ";
+                getline(cin, destination);
+                max_trains = graph.maxFlow(source, destination);
+                if (max_trains != -1) cout << "\nMaximum number of trains that can simultaneously travel: " << max_trains << endl;
+                break;
+            case 3:
                 return;
             default:
                 cout << "Please, select a valid option!" << endl;
